50a32b54deb2ebaffe8f034dc116a9200f0143f2
Emmanuel Olivier <e_oph@yahoo.fr>
2025-09-10 15:20:38 +0200

    add cors config

---
+++ spring-infra/src/main/kotlin/org/flexstore/infra/spring/CorsConfig.kt
@@ -0,0 +1,18 @@
+package org.flexstore.infra.spring
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer = object : WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry.addMapping("/api/**")
+                .allowedOriginPatterns("https://*.flexstore-front.pages.dev")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+        }
+    }
+}
