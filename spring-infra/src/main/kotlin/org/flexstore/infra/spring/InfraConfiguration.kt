30190a081217fead25038b6551f07b86da857294
Emmanuel Olivier <e_oph@yahoo.fr>
2025-09-15 22:07:34 +0200

    add ProductController

--- spring-infra/src/main/kotlin/org/flexstore/infra/spring/InfraConfiguration.kt
+++ spring-infra/src/main/kotlin/org/flexstore/infra/spring/InfraConfiguration.kt
@@ -1,9 +1,11 @@
 package org.flexstore.infra.spring
 
 import org.flexstore.domain.repository.ItemRepository
+import org.flexstore.domain.repository.ProductRepository
 import org.flexstore.domain.repository.UserRepository
 import org.flexstore.domain.service.UserService
 import org.flexstore.infra.repository.InMemoryItemRepository
+import org.flexstore.infra.repository.InMemoryProductRepository
 import org.flexstore.infra.repository.InMemoryUserRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -24,5 +26,10 @@ open class InfraConfiguration {
     @Bean
     open fun itemRepository(): ItemRepository {
         return InMemoryItemRepository()
+    }
+
+    @Bean
+    open fun productsRepository(): ProductRepository {
+        return InMemoryProductRepository()
     }
 }
---
+++ spring-infra/src/test/kotlin/org/flexstore/infra/spring/rest/ProductControllerTest.kt
@@ -0,0 +1,41 @@
+package org.flexstore.infra.spring.rest
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    private lateinit var productController: ProductController
+
+    @Test
+    fun `should returnØ products`() {
+        val mvcResult = mockMvc.perform(get("/api/products/"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+        val responseContent = mvcResult.response.contentAsString
+        val products: List<JsonProduct> = objectMapper.readValue(
+            responseContent,
+            objectMapper.typeFactory.constructCollectionType(List::class.java, JsonProduct::class.java)
+        )
+        assertThat(products).isNotEmpty
+    }
+}
