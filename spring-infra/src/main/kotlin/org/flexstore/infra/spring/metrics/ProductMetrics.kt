8cac208a254d611e18045123f8e8c1059b1c9afc
Emmanuel Olivier <e_oph@yahoo.fr>
2025-11-02 22:42:09 +0100

    try to fix prometheus config

--- spring-infra/src/main/kotlin/org/flexstore/infra/spring/metrics/ProductMetrics.kt
+++ spring-infra/src/main/kotlin/org/flexstore/infra/spring/metrics/ProductMetrics.kt
@@ -1,13 +1,12 @@
 package org.flexstore.infra.spring.metrics
 
 import io.micrometer.core.instrument.Counter
-import org.slf4j.LoggerFactory
+import io.micrometer.core.instrument.Gauge
 import org.springframework.stereotype.Component
 
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tag
 import io.micrometer.core.instrument.Timer
-import jakarta.annotation.PostConstruct
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -20,28 +19,28 @@ private const val FLEXSTORE_INVENTORY_TOTAL = "flexstore.inventory.total"
 @Component
 class ProductMetrics(private val registry: MeterRegistry) {
 
-    private val log = LoggerFactory.getLogger(javaClass)
-    //private val productCreatedCounter = registry.counter(FLEXSTORE_PRODUCTS_CREATED)
+    private val inventory = AtomicInteger(0)
+
     private val productCreatedCounter =  Counter.builder(FLEXSTORE_PRODUCTS_CREATED)
         .description("Number of products created")
         .register(registry)
-    private val inventoryGaugeValue = AtomicInteger(0)
+
     private val listTimer: Timer = Timer.builder(FLEXSTORE_PRODUCTS_LIST_TIMER)
         .description("Time to list products")
         .publishPercentiles(0.5, 0.95, 0.99)
         .publishPercentileHistogram()
-        .register(registry)
+        .register(registry) // -> flexstore_products_list_timer_seconds_*
 
     init {
-        registry.gauge(FLEXSTORE_INVENTORY_TOTAL, listOf(Tag.of("unit", "items")), inventoryGaugeValue)
+        Gauge
+            .builder(FLEXSTORE_INVENTORY_TOTAL, inventory) { it.get().toDouble() }
+            .description("Total number of products")
+            .baseUnit("items")
+            .register(registry) // -> flexstore_inventory_total
     }
 
-    @PostConstruct
-    fun logRegistry() {
-        log.info("MeterRegistry in ProductMetrics = {}", registry::class.qualifiedName)
-    }
 
     fun incrementCreated() = productCreatedCounter.increment()
     fun recordList(durationNanos: Long) = listTimer.record(durationNanos, TimeUnit.NANOSECONDS)
-    fun setInventory(total: Int) { inventoryGaugeValue.set(total) }
+    fun setInventory(total: Int) { inventory.set(total) }
 }
