cc4c68f6668f3fd1481f7290ff023aa9a86cbd38
Emmanuel Olivier <e_oph@yahoo.fr>
2025-11-20 15:06:14 +0100

    fix db config

--- backend/spring-infra/src/main/kotlin/org/flexstore/infra/spring/DebugEnv.kt
+++ backend/spring-infra/src/main/kotlin/org/flexstore/infra/spring/DebugEnv.kt
@@ -2,12 +2,13 @@ package org.flexstore.infra.spring
 
 import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 
-class DebugEnv(
-    @Value("\${FLEXSTORE_DATASOURCE_INTERNAL_HOST:NOT_FOUND}") private val host: String
+@Component open class DebugEnv(
+    @Value("\${FLEXSTORE_DATABASE_URL:NOT_FOUND}") private val url: String
 ) {
     @PostConstruct
     fun printEnv() {
-        println(">>> FLEXSTORE_DATASOURCE_INTERNAL_HOST = $host")
+        println(">>> FLEXSTORE_DATABASE_URL = $url")
     }
 }
