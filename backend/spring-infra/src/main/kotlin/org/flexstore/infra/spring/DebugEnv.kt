9b062c44c6cd4a6099d24fe42b326ff1e8345db5
Emmanuel Olivier <e_oph@yahoo.fr>
2025-12-01 11:43:56 +0100

    update spring config for local run

--- backend/spring-infra/src/main/kotlin/org/flexstore/infra/spring/DebugEnv.kt
+++ backend/spring-infra/src/main/kotlin/org/flexstore/infra/spring/DebugEnv.kt
@@ -5,10 +5,16 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component open class DebugEnv(
-    @Value("\${FLEXSTORE_DATABASE_URL:NOT_FOUND}") private val url: String
+    @Value("\${FLEXSTORE_DATASOURCE_HOST:NOT_FOUND}") private val host: String,
+    @Value("\${FLEXSTORE_DATASOURCE_HOST_SUFFIX:NOT_FOUND}") private val hostSuffix: String,
+    @Value("\${FLEXSTORE_DATASOURCE_PORT:NOT_FOUND}") private val port: String,
+    @Value("\${FLEXSTORE_DATASOURCE_DB:NOT_FOUND}") private val db: String
 ) {
     @PostConstruct
     fun printEnv() {
-        println(">>> FLEXSTORE_DATABASE_URL = $url")
+        println(">>> FLEXSTORE_DATASOURCE_HOST = $host")
+        println(">>> FLEXSTORE_DATASOURCE_HOST_SUFFIX = $hostSuffix")
+        println(">>> FLEXSTORE_DATASOURCE_PORT = $port")
+        println(">>> FLEXSTORE_DATASOURCE_DB = $db")
     }
 }
